6039d8004c0325955dc4df012954d7b9a248384e
brodilin ivan <brodilinivan@yandex.ru>
2023-02-07 19:17:35 +0400

    1 задача решена

--- DZ/task1.py
+++ DZ/task1.py
@@ -1,7 +1,8 @@
 """
 Задание 1.
 Создать класс TrafficLight (светофор)
-и определить у него один приватный атрибут color (цвет) и публичный метод running (запуск).
+и определить у него один приватный атрибут color (цвет) и публичный метод
+running (запуск).
 В рамках метода running реализовать переключение светофора в режимы:
 красный, желтый, зеленый. Продолжительность первого состояния (красный)
 составляет 7 секунд, второго (желтый) — 2 секунды, третьего (зеленый) —
@@ -11,3 +12,23 @@
 в указанном порядке (красный, желтый, зеленый).
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+from time import sleep
+class TrafficLight:
+
+    def __init__(self, color, times):
+        self._color = color
+        self.times = times
+
+    def running(self):
+        print(f'Горит {self._color} свет')
+        sleep(self.times)
+
+
+red = TrafficLight('Красный', 7)
+yellow = TrafficLight('Желтый', 2)
+green = TrafficLight('Зеленый', 4)
+
+red.running()
+yellow.running()
+green.running()
